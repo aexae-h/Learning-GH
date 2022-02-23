442b3ff93f497a0920f5db94384dfeccca52fc9b
Alexander Hader <alexha97@googlemail.com>
2022-02-23 11:22:44 +0100

    Initial Commit

---
+++ m3_learning_GH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  m3_learning_GH
+//
+//  Created by Alexander Hader on 23.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ m3_learning_GH/m3_learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  m3_learning_GHApp.swift
+//  m3_learning_GH
+//
+//  Created by Alexander Hader on 23.02.22.
+//
+
+import SwiftUI
+
+@main
+struct m3_learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
